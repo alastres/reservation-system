3d87f3126742769f3cea63dc91f69bebf982b2c5
Alas3s <alas3spaypal@gmail.com>
2026-01-21 11:22:22 +0100

    implementando toogle theme, webhook, y stripe

---
+++ lib/stripe-client.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+
+let stripePromise: Promise<Stripe | null>;
+
+export const getStripe = () => {
+    if (!stripePromise) {
+        const key = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+
+        if (!key) {
+            console.error("NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY is not defined");
+            return null;
+        }
+
+        stripePromise = loadStripe(key);
+    }
+
+    return stripePromise;
+};
